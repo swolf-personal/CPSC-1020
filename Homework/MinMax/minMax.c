c5ecacfbc8ab9ce47c2de0a48b38ae0b7a974d95
Stephen Wolf <46688140+swolf-personal@users.noreply.github.com>
2020-05-20 20:44:14 -0400

    Finished MinMax

--- Homework/MinMax/minMax.c
+++ Homework/MinMax/minMax.c
@@ -2,7 +2,18 @@
 
 void findMinMax(int* arr, int size)
 {
-    /*Your code goes here */
+    int tempMax = -100000, tempMin = 100000;
+    for(int i = 0; i < size; i++) {
+      int testSum = 0;
+      for(int j = 0; j < size; j++)
+        if (i != j)
+          testSum += arr[j]; 
+      if(testSum < tempMin)
+        tempMin = testSum;
+      if (testSum > tempMax)
+        tempMax = testSum;
+    }
+    printf("%d %d\n", tempMin, tempMax);
 }
 
 int* readData(FILE* in, int* sz)

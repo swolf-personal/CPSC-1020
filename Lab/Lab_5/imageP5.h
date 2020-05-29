55979731c5b74e66b9af6a2021a78c542626b442
Stephen Wolf <46688140+swolf-personal@users.noreply.github.com>
2020-05-28 21:17:05 -0400

    Real finish lab 5

--- Lab/Lab_5/imageP5.h
+++ Lab/Lab_5/imageP5.h
@@ -34,11 +34,30 @@ class ImageP5{
 
         vector<vector<unsigned char>> pixels;
 
+        /* INPUT : Two points
+           OUTPUT : the integer distance between the two points
+           Takes two point structs and returns the distance between them
+        */
         int calcDist(Point, Point);
+        /* INPUT : The distance between two points
+           OUTPUT : The value (0-255) of the pixel based on distance
+           Using the distance from calcDist this func returns the correct greyscale for a corner gradient
+        */
         int calcGreyscale(int);     
     
     public:
-        ImageP5(int, int);     
-        void fill();      
+        /* INPUT : Width and Height both integers that define the bounds of the image
+           OUTPUT : None
+           Also populates the object with a start (0,0) and end (max of both bounds)
+        */
+        ImageP5(int, int);
+        /* INPUT : None
+           OUTPUT : None
+           Fills internal pixel vector with a greyscale gradient 
+        */     
+        void fill(); 
+        /* INPUT : Outfile stream object
+           OUTPUT : Prints the image object to the given stream
+        */      
         void print(ofstream&);
 };
